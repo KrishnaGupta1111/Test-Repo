d6297e042a9cd14cf3dd84b20a55e933b2f10804
Krishna Gupta <krishnaguptaa44@gmail.com>
2025-06-21 17:58:09 +0530

    Added Admin Layout

--- client/src/pages/admin/ListBookings.jsx
+++ client/src/pages/admin/ListBookings.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyBookingData } from '../../assets/assets';
+import Title from '../../components/admin/Title';
+import Loading from '../../components/Loading';
 
 const ListBookings = () => {
-  return (
-    <div>
-      
-    </div>
+
+  const currency=import.meta.env.VITE_CURRENCY 
+
+  const[bookings,setBookings]=useState([]);
+  const[isLoading,setIsLoading]=useState(true);
+
+  const getAllBookings=async()=>{
+    setBookings(dummyBookingData)
+    setIsLoading(false);
+  };
+
+  useEffect(()=>{
+    getAllBookings();
+  },[])
+
+  return !isLoading ? (
+   <>
+   <Title text1="List" text2="Bookings"/>
+   <div className='max-w-4xl mt-6 overflow-x-auto'>
+    <table className='w-full border-collapse  rounded-md overflow-hidden text-nowrap'>
+      <thead>
+        <tr className='bg-primary/20 text-left text-white'>
+        <th className='p-2 font-medium pl-5'>User Name</th>
+        <th className='p-2 font-medium '>Movie Name</th>
+        <th className='p-2 font-medium '>Show Time</th>
+        <th className='p-2 font-medium '>Seats</th>
+        <th className='p-2 font-medium '>Amount</th>
+
+        </tr>
+      </thead>
+    </table>
+   </div>
+   </>
+   
+  ) : (
+    <Loading/>
   )
 }
+
 
 export default ListBookings
