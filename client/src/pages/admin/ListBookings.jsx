8a43aab9a2e17b5c5ea06ba515132de85d487e30
Krishna Gupta <krishnaguptaa44@gmail.com>
2025-06-22 20:36:14 +0530

    Added AddShows and ListBookings Admin Page

--- client/src/pages/admin/ListBookings.jsx
+++ client/src/pages/admin/ListBookings.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { dummyBookingData } from '../../assets/assets';
 import Title from '../../components/admin/Title';
 import Loading from '../../components/Loading';
+import { dateFormat } from '../../lib/dateFormat';
 
 const ListBookings = () => {
 
@@ -34,6 +35,17 @@ const ListBookings = () => {
 
         </tr>
       </thead>
+      <tbody className='text-sm font-light'>
+        {bookings.map((item,index)=>(
+          <tr key={index} className='border-b border-primary/20 bg-primary/5 even:bg-primary/10'>
+            <td className='p-2 min-w-45 pl-5'>{item.user.name}</td>
+            <td className='p-2 min-w-45 pl-5'>{item.show.movie.title}</td>
+            <td className='p-2'>{dateFormat(item.show.showDateTime)}</td>
+            <td className='p-2'>{Object.keys(item.bookedSeats).map(seat=>item.bookedSeats[seat]).join(", ")}</td>
+            <td className='p-2'>{currency}{item.amount}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
    </div>
    </>
